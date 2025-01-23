be91ae566b35281b5bff5f3e50c208f750556ed5
ANireshkumar <niresh1606@gmail.com>
2025-01-23 05:41:15 +0530

    video:2,JS and JSX,header component and added className,expression,customCSS,line styling,rendering,List(map)

--- vite-project/src/Components/Header.jsx
+++ vite-project/src/Components/Header.jsx
@@ -1,6 +1,16 @@
 
 
 export const Header = () => {
+
+let customCSS = "code";
+
+
+const isLoggIn = true;
+
+const greeting = isLoggIn ? <p>Welcome back !!</p> :  <p>Please login!</p>
+
+const items = ["Item1", "Item2", "Item3","Item4"];
+
   return (
  <>
  
@@ -10,7 +20,35 @@ export const Header = () => {
 
 <p className="slogan">This is the second component named as the Header</p>
 
+<h2>
+JavaScript expression in JSX: and dynamic styling </h2>
+
+<p>25+25 = {25+25}</p>
+
+<p>25-5 = {25-25}</p>
+
+<p className={customCSS}>dynamic styling</p>
+
+<p className={customCSS}>dynamic styling</p>
+
+<p className={customCSS}>dynamic styling</p>
+
+
+<p style={{fontSize:"50px", color: "red"}}> This is in-line styling</p>
  </div>
+
+<h3> Conditional rendering</h3>
+
+{greeting}
+
+<h3>JSX with List</h3>
+
+<h3>JSX with list</h3>
+<ul>
+{items.map((item , index) => (<li key = {item}>{item}</li>))}
+
+</ul>
+
  </>
   )
 }
